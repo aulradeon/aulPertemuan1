28f841c7df1bc8f809e564cc4d8e5ec7c7029589
aulradeon <rizqims700@gmail.com>
2025-07-17 09:14:44 +0700

    add icons, change default font to inter

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link"
+import Image from "next/image"
 
 function Header() {
-  const luping = ["home", "blogs", "partners", "contacts","about"]
+  const luping = ["home", "blogs", "partners", "contacts", "about"]
   const headerTitle = "NeonTech"
   return (
-    <header className="flex gap-4 p-2.5 bg-white">
-      <img alt="nope" />
-      <h1 className="text-xl font-bold bg-gradient-to-r from-[#ad0e8d] to-[#7d00d1] bg-clip-text text-transparent">{headerTitle}</h1>
+    <header className="flex gap-2 p-2.5 bg-white">
+      <Image
+        src={"/plump-gradient.png"}
+        width={48}
+        height={48}
+        alt="nope"
+      />
+      <h1 className="self-center text-xl font-bold bg-gradient-to-r from-[#ad0e8d] to-[#7d00d1] bg-clip-text text-transparent">{headerTitle}</h1>
       <nav className='self-center ml-auto mr-4'>
         <ul className="flex flex-row gap-5 justify-end">
           {luping.map(num => <li key={`${num}`}><Link className='text-sm text-[#7d00d1]' href={num == "home" ? '/' : `${num}`}>{num.charAt(0).toUpperCase() + num.slice(1)} </Link></li>)}
@@ -15,6 +21,5 @@ function Header() {
     </header>
   )
 }
-
 
 export default Header
